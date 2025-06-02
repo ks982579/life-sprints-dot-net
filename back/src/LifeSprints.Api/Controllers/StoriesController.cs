16ae93b736f5a0cebed38a18a9261d4f6dbbaeeb
Kevin Sullivan <kevin.sullivan@sullivansoftware.dev>
2025-06-02 12:25:12 +0100

    beginning work on controllers

---
+++ back/src/LifeSprints.Api/Controllers/StoriesController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using LifeSprints.Data;
+using LifeSprints.Models;
+
+namespace LifeSprints.Api.Controllers
+{
+    // Microsoft.AspNetCore.Mvc::ControllerBase
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StoriesController : ControllerBase
+    {
+        // ...
+    }
+}
